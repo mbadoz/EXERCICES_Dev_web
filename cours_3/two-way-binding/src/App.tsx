c89a35a6313ba31556ee0d2cfbabbf3743b9de9f
Marius <marius.badoz@efrei.net>
2026-02-13 12:20:32 +0100

    partie 3

--- cours_3/two-way-binding/src/App.tsx
+++ cours_3/two-way-binding/src/App.tsx
@@ -1,23 +1,26 @@
 import Review from './Review';
+import { useState } from 'react';
 
 function App() {
+    const [feedback, setFeedback] = useState('');
+    const [student, setStudent] = useState('');
   return (
     <>
       <section id="feedback">
         <h2>Please share some feedback</h2>
         <p>
           <label>Your Feedback</label>
-          <textarea />
+          <textarea value={feedback} onChange={(e) => setFeedback(e.target.value)}/>
         </p>
         <p>
           <label>Your Name</label>
-          <input type="text" />
+          <input type="text" value={student} onChange={(e) => setStudent(e.target.value)}/>
         </p>
       </section>
       <section id="draft">
         <h2>Your feedback</h2>
 
-        <Review />
+        <Review  feedback={feedback} student={student}/>
 
         <p>
           <button>Save</button>
